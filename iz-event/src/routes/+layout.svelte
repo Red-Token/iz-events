3f55c87914e755e39c92ee0176121752f8727a07
ightson <ightson@gmail.com>
2025-01-04 20:36:14 +0300

    fix

---
+++ iz-event/src/routes/+layout.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { page, navigating } from '$app/state';
+	let { children } = $props();
+	import '../static/app.css';
+</script>
+
+<main>
+
+	<nav>
+		<a href="/home" aria-current={page.url.pathname === '/home'}> home </a>
+		<a href="/create" aria-current={page.url.pathname === '/create'}> create </a>
+		<a href="/about" aria-current={page.url.pathname === '/about'}> about </a>
+		<a href="/event" aria-current={page.url.pathname === '/event'}> event </a>
+		
+		{#if navigating.to}
+		navigating to {navigating.to.url.pathname}
+		{/if}
+	</nav>
+	
+</main>
+{@render children()}
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 100%;
+		margin: 0 auto;
+	}
+	nav {
+		width: 100%;
+		font-size: 18px;
+		text-align: center;
+		margin-top: 2rem;
+		text-align: center;
+		padding: 1rem 0;
+		margin-left: 0 rem;
+	}
+	nav a {
+		display: inline-block;
+		padding: 0 1rem;
+		border-left: 1px solid var(--color-border);
+	}
+
+	nav a:first-of-type {
+		border: 0;
+	}
+</style>
